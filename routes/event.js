cc3fc135f35620a7b4b6a019749f4709a3bd55ad
JeZoS <prabhat262111@gmail.com>
2021-02-08 23:43:28 +0530

    comments

--- routes/event.js
+++ routes/event.js
@@ -9,7 +9,6 @@ var storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    // console.log(req);
     var filname =
       file.originalname +
       new Date().toISOString() +
@@ -28,26 +27,26 @@ const upload = multer({ storage: storage });
 //
 //
 //get Event  GET ----localhost:port/event/:id
-router.get("/:id", async (req, res) => {
-  try {
-    const event = await Event.findById(req.params.id);
-    if (event) {
-      res.send({
-        message: "Success",
-        event: event,
-      });
-    } else {
-      res.send({
-        error: "Failed : event no found",
-      });
-    }
-  } catch (err) {
-    console.log(err);
-    res.send({
-      error: err,
-    });
-  }
-});
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const event = await Event.findById(req.params.id);
+//     if (event) {
+//       res.send({
+//         message: "Success",
+//         event: event,
+//       });
+//     } else {
+//       res.send({
+//         error: "Failed : event no found",
+//       });
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     res.send({
+//       error: err,
+//     });
+//   }
+// });
 
 //
 //
@@ -74,10 +73,6 @@ router.post(
         }
       });
     }
-    // if (req.body.moderator) {
-    //   var len = req.body.moderator.length;
-    //   console.log(len);
-    // }
     console.log(req.body);
     res.send({ ok: "ok" });
     // try {
@@ -117,55 +112,55 @@ router.post(
 //
 //
 //Update Event  POST ----localhost:port/event/:id
-router.post("/:id", async (req, res) => {
-  const event = await Event.findById(req.params.id);
-  if (event) {
-    try {
-      event.title = req.body.title || event.title;
-      event.reg_link = req.body.reg_link || event.reg_link;
-      event.when = req.body.when || event.when;
-      event.about_ev = req.body.about_ev || event.about_ev;
-      event.speakers = req.body.speakers || event.speakers;
-      event.moderator = req.body.moderator || event.moderator;
-      event.material = req.body.moderator || event.moderator;
-      event.joining_info = req.body.joining_info || event.joining_info;
-      event.organised_by = req.body.organised_by || event.organised_by;
-      event.tags = req.body.tags || event.tags;
+// router.post("/:id", async (req, res) => {
+//   const event = await Event.findById(req.params.id);
+//   if (event) {
+//     try {
+//       event.title = req.body.title || event.title;
+//       event.reg_link = req.body.reg_link || event.reg_link;
+//       event.when = req.body.when || event.when;
+//       event.about_ev = req.body.about_ev || event.about_ev;
+//       event.speakers = req.body.speakers || event.speakers;
+//       event.moderator = req.body.moderator || event.moderator;
+//       event.material = req.body.moderator || event.moderator;
+//       event.joining_info = req.body.joining_info || event.joining_info;
+//       event.organised_by = req.body.organised_by || event.organised_by;
+//       event.tags = req.body.tags || event.tags;
 
-      const updatedEvent = await event.save();
-      res.send({
-        msg: "Event Updated",
-        event: updatedEvent,
-      });
-    } catch (err) {
-      console.log(err);
-      res.send({
-        error: err,
-      });
-    }
-  } else {
-    res.send({
-      error: "No event with this title",
-    });
-  }
-});
+//       const updatedEvent = await event.save();
+//       res.send({
+//         msg: "Event Updated",
+//         event: updatedEvent,
+//       });
+//     } catch (err) {
+//       console.log(err);
+//       res.send({
+//         error: err,
+//       });
+//     }
+//   } else {
+//     res.send({
+//       error: "No event with this title",
+//     });
+//   }
+// });
 
-//
-//
-//Delete event  POST ----localhost:port/event/:id
-router.delete("/:id", async (req, res) => {
-  try {
-    const event = await Event.findById(req.params.id);
-    await event.delete();
-    res.send({
-      message: "Event deleted",
-    });
-  } catch (err) {
-    console.log(err);
-    res.send({
-      error: "err",
-    });
-  }
-});
+// //
+// //
+// //Delete event  POST ----localhost:port/event/:id
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const event = await Event.findById(req.params.id);
+//     await event.delete();
+//     res.send({
+//       message: "Event deleted",
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.send({
+//       error: "err",
+//     });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
